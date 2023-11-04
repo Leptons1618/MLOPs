25c1556fa87f5f75f6b98c77ec90427fe2add667
Leptons1618 <anishgiri163@gmail.com>
2023-11-04 20:37:44 +0530

    Add test_model.py and update Dockerfile and README.md

--- train_model.py
+++ train_model.py
@@ -12,10 +12,14 @@ y = data.target  # Target labels (0 for setosa, 1 for versicolor, 2 for virginic
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Initialize and train the model
-model = RandomForestClassifier(n_estimators=100)
-model.fit(X_train, y_train)
+# Train the model
+def train_model(X_train, y_train):
+    # Initialize and train the model
+    model = RandomForestClassifier(n_estimators=100)
+    model.fit(X_train, y_train)
+    return model
 
-# Model evaluation and saving
-accuracy = model.score(X_test, y_test)
-print(f'Model accuracy: {accuracy}')
+if __name__ == '__main__':
+    model = train_model(X_train, y_train)
+    accuracy = model.score(X_test, y_test)
+    print(f'Model accuracy: {accuracy}')
